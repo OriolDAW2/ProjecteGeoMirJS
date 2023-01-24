c985fae3c33e1dce9f2328c0f5eb27864faedb49
Oriol <ormoin@fp.insjoaquimmir.cat>
2023-01-24 16:49:32 +0100

    Parte 4 acabada (Falta CSS)

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -3,6 +3,8 @@ import { UserContext } from "./userContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import About from "./About";
+import Places from "./Places/Places";
+import Posts from "./Posts/Posts";
 import Header from "./Layout/Header";
 import Footer from "./Layout/Footer";
 import LoginRegister from './auth/LoginRegister'
@@ -23,6 +25,8 @@ function App() {
           <Router>
             <Header />
             <Routes>
+              <Route path="/places" element={<Places />} />
+              <Route path="/posts" element={<Posts />} />
               <Route path="/about" element={<About />} />
             </Routes>
             <Footer />
